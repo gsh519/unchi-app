595ddb677019b14b36f5bd3d884a3e9a86b053cb
gsh519 <gsh05196yu@yahoo.co.jp>
2022-01-12 21:13:29 +0900

    関数化戻し

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once("./function.php");
-
 define('DB_NAME', $_SERVER['DB_NAME']);
 define('DB_HOST', $_SERVER['DB_HOST']);
 define('DB_USER', $_SERVER['DB_USER']);
@@ -18,7 +16,11 @@ $user_name = $_POST['user_name'];
 if (!empty($login)) {
   // データーベースに接続
   try {
-    db_connect(DB_NAME, DB_HOST, DB_USER, DB_PASS);
+    $option = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    ];
+    $pdo = new PDO('mysql:charset=UTF8;dbname=' . DB_NAME . ';host=' . DB_HOST, DB_USER, DB_PASS, $option);
   } catch (PDOException $e) {
     $errors[] = $e->getMessage();
   }
